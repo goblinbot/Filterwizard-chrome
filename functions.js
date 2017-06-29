5deea3e5b70d7040fcfb731979f904fb6eb87c06
webdesigngse <webdesign@gse.nl>
2017-06-29 16:44:31 +0200

    kleine fixes

--- functions.js
+++ functions.js
@@ -1,7 +1,9 @@
+
 function wizardFilter(spell) {
 
   if(spell) {
 
+    /* haalt/target de DOM van actieve tablat */
     chrome.tabs.query ({ currentWindow: true, active: true }, function(tabs) {
       var activeTab = tabs[0];
 
@@ -21,26 +23,32 @@ function wizardFilter(spell) {
 
 $(document).ready(function(){
 
+  /* var voor validatie van spell toggle */
   var currentBuff = "";
 
   chrome.tabs.query ({ currentWindow: true, active: true }, function(tabs) {
    var activeTab = tabs[0];
-     chrome.tabs.executeScript(activeTab.id, {
-       file: 'jquery.min.js'
-     });
-     chrome.tabs.executeScript(activeTab.id, {
-       file: 'functions.js'
-     });
+
+    /* geeft aan de front/DOM de 2 javascripts */
+    chrome.tabs.executeScript(activeTab.id, {
+      file: 'jquery.min.js'
+    });
+      chrome.tabs.executeScript(activeTab.id, {
+    file: 'functions.js'
+    });
   });
 
+  /* checkt of spell al actief is */
   function validateSpell(spell) {
     if(currentBuff == spell) {
       spell = 'none';
     }
+    /* call de echte functie */
     wizardFilter(spell);
     currentBuff = spell;
   }
 
+  /* event on clicks: moet helaas via event ipv gewoon onclick want security. */
   $('#invertSpell').click(function(){
     validateSpell('invert(100%)');
   });
@@ -57,10 +65,11 @@ $(document).ready(function(){
     validateSpell('blur(4px)');
   });
 
-    $('#clearSpell').click(function(){
-      wizardFilter('none');
-      currentBuff = 'none';
-    });
+  /* reset knop */
+  $('#clearSpell').click(function(){
+    wizardFilter('none');
+    currentBuff = 'none';
+  });
 
 
 });
